6aa2d1e426c66b4d57ff6b3429624b9cb7296106
Saad-04 <asaad4674@gmail.com>
2023-06-11 03:08:48 +0500

    third

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {createStore ,applyMiddleware} from 'redux';
+import {createStore ,applyMiddleware,combineReducers} from 'redux';
 import logger from 'redux-logger';``
 import axios from 'axios';
 import thunk from 'redux-thunk';
@@ -7,10 +7,13 @@ const inc = 'increment';
 const dec = 'decrement'
 const init = 'init'
 const actionbyAmount = 'incrementBypayload'
-const store = createStore(reducer,applyMiddleware(logger.default,thunk.default)); //thunk is used to hold the funtion to get the data with async awati funciotn 
-const previous = []
+const store = createStore(combineReducers({
+    amount:amoutReducer,
+    bonus:bonusReducer,
+}),applyMiddleware(logger.default,thunk.default)); //thunk is used to hold the funtion to get the data with async awati funciotn 
+// const previous = []
 
-function reducer(state={amount:1},action){
+function amoutReducer(state={amount:1},action){
 
 switch (action.type) {
     case inc: return {amount:state.amount+ 1}
@@ -21,6 +24,16 @@ switch (action.type) {
         break;
     case init: return {amount: action.payload}
         break;
+    default: return state
+        break;
+}  
+}
+function bonusReducer(state={points:1},action){
+
+switch (action.type) {
+    case inc: return {points:state.points+1}
+        break;
+    case actionbyAmount: if(action.payload >=100) {return {points:state.points+1}}
     default: return state
         break;
 }  
@@ -43,14 +56,15 @@ function incrementByPayload(pl){
 }
 
 // async api calling 
-async function getUser(dispatch,getState){
-    const {data} = await axios.get('http://localhost:3000/account/2')
+function getUserAcount(id){
+    return async(dispatch,getState)=>{
+        const {data} = await axios.get(`http://localhost:3000/account/${id}`)
     dispatch ({type:init,payload:data.amount})
-    
+       }
 }
-
 setTimeout(() => {
-    store.dispatch(getUser) //dispatch the function for action type 
+    // store.dispatch(getUserAcount(1)) //dispatch the function for action type 
+    store.dispatch(incrementByPayload(200)) 
 }, 1000);
 
 
